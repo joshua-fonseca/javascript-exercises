2c1758d8d32388b4cee6b3d5cffdc66ba6533a57
joshua-fonseca <joshuacfonseca@gmail.com>
2025-05-05 10:54:11 -0400

    completed exercise 9
    note: learned about array de-structuring
    different from swapping variables with clever arithmetic, or even a temp variable

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
 
+    if(!Number.isInteger(x) || !Number.isInteger(y)
+    || x < 0 || y < 0) return 'ERROR';
+
+    if(x > y) [x, y] = [y, x];
+    
+
+    for (let i=x; i<=y; i++) {
+        // console.log(`i: ${i} x: ${x}`);
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
