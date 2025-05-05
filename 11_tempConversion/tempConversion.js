85804ab97ac802b9949e03f854a065e9c97511ca
joshua-fonseca <joshuacfonseca@gmail.com>
2025-05-05 11:26:29 -0400

    completed exercise 11 last of exercises for foundations-arrays-and-loops

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let converted = (tempInF - 32) / (9/5);
+  return Math.round(converted * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let converted = tempInC * (9/5) + 32;
+  return Math.round(converted * 10) / 10;
 };
 
 // Do not edit below this line
