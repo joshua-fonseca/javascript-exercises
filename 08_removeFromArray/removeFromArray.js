ea22cfc6dcc4248ecd499a2e79a783b86e338bd4
joshua-fonseca <joshuacfonseca@gmail.com>
2025-05-05 10:22:41 -0400

    completed exercise 7 proud i did it the "advanced way" using filter :)

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+// v1
+// const removeFromArray = function(array, x) {
+//     array.splice(array.indexOf(x), 1)
+//     return array;
+// };
+
+const removeFromArray = function(array, ...args) {
+    argsArray = Array.from(args);
+    // const result = array.filter((x) => !argsArray.includes(x));
+    // return result;
+    return array.filter((x) => !argsArray.includes(x));
 };
 
 // Do not edit below this line
