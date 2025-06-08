b0fa309ac6c15707bd2eaf5a5f0e221b933358a2
joshua-fonseca <joshuacfonseca@gmail.com>
2025-06-08 01:26:11 -0400

    completed exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (sen) {
+    let clean = sen
+        .split('')
+        .map(x => x.toLowerCase())
+        .filter(x => (
+            x !== ' ' &&
+            ((x.toUpperCase() != x.toLowerCase()) || !isNaN(x)))
+        );
 
+    return clean.join('') === clean.toReversed().join('') ? true : false;
 };
 
 // Do not edit below this line
