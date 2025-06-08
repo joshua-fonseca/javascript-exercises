25b8e7e492d45daf0bcdaf20089000150cb4a172
joshua-fonseca <joshuacfonseca@gmail.com>
2025-06-08 00:13:43 -0400

    comlete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, curr) => {
+    return sum + curr;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  // return a * b;
+  return arr.reduce((prod, curr) => prod * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(x, n) {
+	return x**n;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0) {
+    return 1;
+  } else {
+    return x * factorial(x-1);
+  }
 };
 
 // Do not edit below this line
