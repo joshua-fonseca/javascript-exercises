eefdf95e3735e982b2f5f807e58da3d67e3a7316
joshua-fonseca <joshuacfonseca@gmail.com>
2025-06-09 21:39:45 -0400

    completed exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n = parseInt(n);
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  if (n < 0) return 'OOPS';
+  
+  let f0 = 0;
+  let f1 = 1;
+  let fn;
+  for (let i=0; i<n-1; i++) {
+    fn = f0 + f1;
+    f0 = f1;
+    f1 = fn;
+  }
+  return fn;
 };
 
 // Do not edit below this line
